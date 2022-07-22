866defc48c0d8632e2a403deec26f18b3390ddcd
benegg <lh@hust.edu.cn>
2022-07-22 14:40:24 +0800

    NO THING CHANGED, JUST HAVE A TRY

--- 扰动生成算法/盲扰动_pytorch/family/test.py
+++ 扰动生成算法/盲扰动_pytorch/family/test.py
@@ -3,6 +3,9 @@ from torch.autograd import Variable
 import numpy as np
 import pickle
 
+
+
+
 with open("iter_dataset/markov_universal_1000_trainLoop2000_1_1000","rb") as f:
     data=pickle.load(f)
 
